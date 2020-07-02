32e911a75b191a645dcd728438a33d6b3c99f61a
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-02 18:27:04 +0200

    Update update_paramters.m

--- OPLF/update_paramters.m
+++ OPLF/update_paramters.m
@@ -1,4 +1,8 @@
 function [eta, sigma, P, gamma] = update_parameters(eta, sigma, P, u, gamma, lambda, s)
+% Avoid possible inestabilities of matrix P
+if norm(P) > 5
+    P = eye(length(P));
+end
 P = (1/lambda)*(P - (P*u*u'*P)/(lambda + u'*P*u));
 gamma = 1 + lambda*gamma;
 sigma = sqrt(sigma^2 - (1/gamma)*(sigma^2 - lambda*(s - u'*eta)^2)/(lambda + u'*P*u));
