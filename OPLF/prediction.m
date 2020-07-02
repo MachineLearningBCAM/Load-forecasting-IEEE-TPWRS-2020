1ca8ea660fd49382827a89e713a60cbedd512fab
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-02 18:25:23 +0200

    Update prediction.m

--- OPLF/prediction.m
+++ OPLF/prediction.m
@@ -1,31 +1,27 @@
-function [pred_s, e] = prediction(s0, etad, sigmad, etar, sigmar, DoW, HoD, wt, w)
-L = length(DoW);
+function [pred_s, e] = prediction(Theta, x, C)
+% x = [s0, w, wt]; (wt is included in Theta)
+L = length(x)-1; % Prediction horizon
 pred_s = zeros(L+1, 1); % Predictions
 e = zeros(L+1, 1); % Estimated errors
 pred_s(1, 1) = s0;
 for i=2:L+1
-    % Weekday -> j = 1
-    % Weekend or holiday -> j = 2
-    if DoW(i-1) >=2 && DoW(i-1) <=6
-        j = 1;
-    else
-        j = 2;
-    end
-    h = HoD(i-1);
+    c = C(i-1);
     ud = [1, pred_s(i-1)]';
-if wt(HoD(i-1), 1) - w(i-1) > 20 && (w(i-1) > 80 || w(i-1) < 20)
+    % Feature vector that represents observations
+if wt(c) - w(i-1) > 20 && (w(i-1) > 80 || w(i-1) < 20)
     alpha1 = 1;
     alpha2 = 0;
-elseif wt(HoD(i-1), 1) - w(i-1) < - 20 && (w(i-1) > 80 || w(i-1) < 20)
+elseif wt(c) - w(i-1) < - 20 && (w(i-1) > 80 || w(i-1) < 20)
     alpha1 = 0;
     alpha2 = 1;
 else
     alpha1 = 0;
     alpha2 = 0;
 end
-ur = [1, alpha1, alpha2]';
-    pred_s(i, 1) = (ud'*etad(:, h, j)*sigmar(h, j)^2 + ur'*etar(:, h, j)*(sigmad(h, j)^2 + ([0, 1]*etad(:, h, j))^2*e(i-1, 1)))/(sigmar(h, j)^2 + sigmad(h, j)^2 + ([0, 1]*etad(:, h, j))^2*e(i-1, 1));
-    e(i,1) = sqrt(((sigmad(h, j)^2 + ([0, 1]*etad(:, h, j))^2*e(i-1, 1))*sigmar(h, j)^2)/(sigmar(h, j)^2 + sigmad(h, j)^2 + ([0, 1]*etad(:, h, j))^2*e(i-1, 1)));
+    ur = [1, alpha1, alpha2]';
+    % Predictions
+    pred_s(i, 1) = (ud'*Theta.etad(:, c)*Theta.sigmar(c)^2 + ur'*Theta.etar(:, c)*(Theta.sigmad(c)^2 + ([0, 1]*Theta.etad(:, c))^2*e(i-1, 1)))/(Theta.sigmar(c)^2 + Theta.sigmad(c)^2 + ([0, 1]*Theta.etad(:, c))^2*e(i-1, 1));
+    e(i,1) = sqrt(((Theta.sigmad(c)^2 + ([0, 1]*Theta.etad(:, c))^2*e(i-1, 1))*Theta.sigmar(c)^2)/(Theta.sigmar(c)^2 + Theta.sigmad(c)^2 + ([0, 1]*Theta.etad(:, c))^2*e(i-1, 1)));
 end
 pred_s(1) = [];
 e(1) = [];
