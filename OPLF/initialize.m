2ac6954090e6e35a3c1864f0ffa84160cc9e8477
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-02 18:24:35 +0200

    Update initialize.m

--- OPLF/initialize.m
+++ OPLF/initialize.m
@@ -1,20 +1,20 @@
-function parameters = initialize(cHoD, cDoW)
+function [Theta, Gamma] = initialize(C, R)
+% C length of calendar information
+% Length of feature vector representation of observations
 % Initialize the parameters
-parameters.etad = zeros(2, cHoD, cDoW);
-parameters.sigmad = zeros(cHoD, cDoW);
-parameters.gammad = ones(cHoD, cDoW);
-parameters.etar = zeros(3, cHoD, cDoW);
-parameters.sigmar = zeros(cHoD, cDoW);
-parameters.gammar = ones(cHoD, cDoW);
-for i = 1:cHoD
-    parameters.Pd(:, :, i, 1) = eye(2);
-    parameters.Pd(:, :, i, 2) = eye(2);
-    parameters.Pr(:, :, i, 1) = eye(3);
-    parameters.Pr(:, :, i, 2) = eye(3);
+Theta.etad = zeros(2, C);
+Theta.sigmad = zeros(C);
+Gamma.gammad = ones(C);
+Theta.etar = zeros(R, C);
+Theta.sigmar = zeros(C);
+Gamma.gammar = ones(C);
+for i = 1:C
+    Gamma.Pd(:, :, C) = eye(2);
+    Gamma.Pr(:, :, C) = eye(R);
 end
 % Mean of the temperature
-parameters.wt = zeros(cHoD, 1);
-parameters.sigmat = zeros(cHoD, 1);
-parameters.gammat = ones(cHoD, 1);
-parameters.Pt = ones(cHoD, 1);
+Theta.wt = zeros(C);
+Theta.sigmat = zeros(C);
+Gamma.gammat = ones(C);
+Gamma.Pt = ones(C);
 end
