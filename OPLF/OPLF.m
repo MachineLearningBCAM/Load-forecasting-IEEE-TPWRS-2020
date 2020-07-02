a3520bc7097ff3bc3a86d362b7d1a9bc2643a457
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-02 18:23:43 +0200

    Update OPLF.m

--- OPLF/OPLF.m
+++ OPLF/OPLF.m
@@ -1,22 +1,26 @@
-function [RMSE, MAPE, predictions, load_demand] = OPLF(data, days_train, lambdad, lambdar, lambdat, L)
+function [RMSE, MAPE, predictions, load_demand] = OPLF(data, days_train, lambdad, lambdar, L, C, R)
 n = length(data.consumption);
 % Initialize the model
 n_train = 24*days_train;
-parameters = initialize(24, 2);
+[Theta, Gamma] = initialize(C, R);
 predictions = [];
+standard_deviation = [];
 load_demand = [];
 for i = 1:L:n_train-L
-    parameters = update_model(parameters, data.consumption(i+1:i+L), data.consumption(i:i+L-1), data.DoW(i+1:i+L), data.HoD(i+1:i+L), lambdad, lambdar, lambdat, data.temperature(i+1:i+L));
+    [Theta, Gamma] = update_model(Theta, Gamma, data.consumption(i+1:i+L), data.consumption(i), data.c(i+1:i+L), lambdad, lambdar, data.temperature(i+1:i+L));
 end
 for j = i+L:L:n - L
     % Prediction
     s0 = data.consumption(j);
-    [pred_s, ~] = prediction(s0, parameters.etad(:, :, :), parameters.sigmad(:, :, :), parameters.etar(:, :, :), parameters.sigmar(:, :, :), data.DoW(j+1:j+L), data.HoD(j+1:j+L), parameters.wt(:, 1), data.temperature(j+1:j+L));
+    w = data.temperature(j+1:j+L);
+    x = [s0, w]; % wt is included in Theta
+    [pred_s, e] = prediction(Theta, x, data.c(j+1:j+L));  % L Load forecasts and probabilistic load forecasts
     predictions = [predictions, pred_s'];
+    standard_deviation = [standard_deviation, e'];
     load_demand = [load_demand, data.consumption(j+1:j+L)'];
     % Learning
-    parameters = update_model(parameters, data.consumption(j+1:j+L), data.consumption(j:j+L-1), data.DoW(j+1:j+L), data.HoD(j+1:j+L), lambdad, lambdar, lambdat, data.temperature(j+1:j+L));
+    [Theta, Gamma] = update_model(Theta, Gamma, data.consumption(j+1:j+L), data.consumption(j), data.c(j+1:j+L), lambdad, lambdar, data.temperature(j+1:j+L));
 end
-% Prediction errors
+% Test
 [MAPE, RMSE] = test(predictions, load_demand);
 end
