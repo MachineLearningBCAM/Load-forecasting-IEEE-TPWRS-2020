e9028ee9b00c4d8aa829b44aeb2b3a6091de3d17
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-11 11:11:05 +0200

    Update initialize.m

--- OPLF/initialize.m
+++ OPLF/initialize.m
@@ -1,20 +1,22 @@
 function [Theta, Gamma] = initialize(C, R)
 % C length of calendar information
-% Length of feature vector representation of observations
-% Initialize the parameters
-Theta.etad = zeros(2, C);
-Theta.sigmad = zeros(C);
-Gamma.gammad = ones(C);
+% R is the length of feature vector representation of observations
+% Initialize model parameters and state variables for each calendar type c = 1, 2, ..., C
+% For each c, etad is a vector with length 2
+Theta.etad = zeros(2, C); 
+Theta.sigmad = zeros(1, C);
+Gamma.gammad = ones(1, C);
+% For each c, etad is a vector with length R
 Theta.etar = zeros(R, C);
-Theta.sigmar = zeros(C);
-Gamma.gammar = ones(C);
+Theta.sigmar = zeros(1, C);
+Gamma.gammar = ones(1, C);
 for i = 1:C
-    Gamma.Pd(:, :, C) = eye(2);
-    Gamma.Pr(:, :, C) = eye(R);
+    Gamma.Pd(:, :, i) = eye(2);
+    Gamma.Pr(:, :, i) = eye(R);
 end
 % Mean of the temperature
-Theta.wt = zeros(C);
-Theta.sigmat = zeros(C);
-Gamma.gammat = ones(C);
-Gamma.Pt = ones(C);
+Theta.wt = zeros(1, C);
+Theta.sigmat = zeros(1, C);
+Gamma.gammat = ones(1, C);
+Gamma.Pt = ones(1, C);
 end
