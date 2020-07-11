89589bdd9053fc69a25c776a8445e03ecb929ac5
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-11 11:10:26 +0200

    Update OPLF.m

--- OPLF/OPLF.m
+++ OPLF/OPLF.m
@@ -1,26 +1,47 @@
 function [RMSE, MAPE, predictions, load_demand] = OPLF(data, days_train, lambdad, lambdar, L, C, R)
+% [RMSE, MAPE, predictions, load_demand] = OPLF(data, 1, 0.2, 0.7, 24, 48, 3);
+% data is a Matlab struct such that:
+    % data.consumption is load demand
+    % data.c is the calendar information
+    % data.temperature is the temperature
+% days_train > 1 is the number of days that we will use for training
+% lambdad = 0.2 forgetting factor 
+% lambdar = 0.7 forgetting factor
+% L = 24 is the prediction horizon
+% C = 48 is the length of the calendar information
+% R = 3 is the length of the feature representation of observations
+% The mean of the temperatures that is included in the instance vector is
+% updated together with model parameters
+% Number of samples
 n = length(data.consumption);
-% Initialize the model
+% Number of hours that we will use for training
 n_train = 24*days_train;
+% Initialize the model (model parameters and state variables)
 [Theta, Gamma] = initialize(C, R);
 predictions = [];
-standard_deviation = [];
+estimated_errors = [];
 load_demand = [];
+n0 = 12;
 for i = 1:L:n_train-L
-    [Theta, Gamma] = update_model(Theta, Gamma, data.consumption(i+1:i+L), data.consumption(i), data.c(i+1:i+L), lambdad, lambdar, data.temperature(i+1:i+L));
+    s0 = data.consumption(i);
+    w = data.temperature(i+1:i+L);
+    x = [s0, w']; % instance vector, wt is included in Theta
+    y = data.consumption(i+1:i+L);
+    [Theta, Gamma] = update_model(Theta, Gamma, y, x, data.c(i+1:i+L), lambdad, lambdar);
 end
-for j = i+L:L:n - L
-    % Prediction
+for j = i+L+1:L:n - L
     s0 = data.consumption(j);
     w = data.temperature(j+1:j+L);
-    x = [s0, w]; % wt is included in Theta
+    x = [s0, w']; % instance vector, wt is included in Theta
+    % Prediction
     [pred_s, e] = prediction(Theta, x, data.c(j+1:j+L));  % L Load forecasts and probabilistic load forecasts
-    predictions = [predictions, pred_s'];
-    standard_deviation = [standard_deviation, e'];
-    load_demand = [load_demand, data.consumption(j+1:j+L)'];
-    % Learning
-    [Theta, Gamma] = update_model(Theta, Gamma, data.consumption(j+1:j+L), data.consumption(j), data.c(j+1:j+L), lambdad, lambdar, data.temperature(j+1:j+L));
+    predictions = [predictions, pred_s']; % List of load forecasts
+    estimated_errors = [estimated_errors, e']; % List of estimated errors
+    % Learning (update model parameters and state variables)
+    y = data.consumption(j+1:j+L);
+    load_demand = [load_demand, y']; % List of load demand 
+    [Theta, Gamma] = update_model(Theta, Gamma, y, x, data.c(j+1:j+L), lambdad, lambdar);
 end
-% Test
+% Prediction errors
 [MAPE, RMSE] = test(predictions, load_demand);
 end
