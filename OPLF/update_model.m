ec6b33577089a6e5d8e1aa3a8aaa6cfb93b627cd
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-11 11:12:35 +0200

    Update update_model.m

--- OPLF/update_model.m
+++ OPLF/update_model.m
@@ -1,12 +1,21 @@
-function [Theta, Gamma] = update_model(Theta, Gamma, y, s0, c, lambdad, lambdar, w)
+function [Theta, Gamma] = update_model(Theta, Gamma, y, x, c, lambdad, lambdar)
+% Theta is the list of model parameters
+% Gamma is the list of state variables
 % y vector of new loads
-% x = [s0, w, wt]; (wt is included in Theta is updated in this step)
+% x = [s0, w]; instance vector (wt is included in Theta is updated in this step)
+% c is the calendar information
+% lambdad, lambdar forgetting factors
+% Prediction horizon
+s0 = x(1);
+w = x(2:end);
 L = length(y);
-y = [s0, y];
+y = [s0, y'];
 for i = 1:L
-    % Mean of temperatures
-    [Theta.wt(c(i)), Theta.sigmat(c(i)), Gamma.Pt(c(i)), Gamma.gammat(c(i))] = update_parameters(Theta.wt(c(i)), Theta.sigmat(c(i)), Gamma.Pt(c(i)), 1, Gamma.gammat(c(i)), 1, w(i));
-    if Gamma.wt(c(i)) - w(i) > 20 && (w(i) > 80 || w(i) < 20)
+    % Update the mean of temperatures with the forgetting factor lambda = 1 and the
+    % feature vector u = 1
+    [Theta.wt(c(i)), Theta.sigmat(c(i)), Gamma.Pt(c(i)), Gamma.gammat(c(i))] = update_parameters(Theta.wt(c(i)), Theta.sigmat(c(i)), Gamma.Pt(c(i)), Gamma.gammat(c(i)), 1, w(i), 1);
+    % Dummy varibles
+    if Theta.wt(c(i)) - w(i) > 20 && (w(i) > 80 || w(i) < 20)
         alpha1 = 1;
         alpha2 = 0;
     elseif Theta.wt(c(i)) - w(i) < - 20 && (w(i) > 80 || w(i) < 20)
@@ -16,9 +25,13 @@ for i = 1:L
         alpha1 = 0;
         alpha2 = 0;
     end
-    ud = [1, y(i-1)]';
-    [Theta.etad(:, c(i)), Theta.sigmad(c(i)), Gamma.Pd(:, :, c(i)), Gamma.gammad(c(i))] = update_parameters(Theta.etad(:, c(i)), Theta.sigmad(c(i)), Gamma.Pd(:, :, c(i)), ud, Gamma.gammad(c(i)), lambdad, y(i));
+    % Feature vector that represents load
+    ud = [1, y(i)]';
+    % Update parameters denoted by d
+    [Theta.etad(:, c(i)), Theta.sigmad(c(i)), Gamma.Pd(:, :, c(i)), Gamma.gammad(c(i))] = update_parameters(Theta.etad(:, c(i)), Theta.sigmad(c(i)), Gamma.Pd(:, :, c(i)), Gamma.gammad(c(i)), lambdad, y(i+1), ud);
+    % Feature vector that represents observations
     ur = [1, alpha1, alpha2]';
-    [Theta.etar(:, c(i)), Theta.sigmar(c(i)), Gamma.Pr(:, :, c(i)), Gamma.gammar(c(i))] = update_parameters(Theta.etar(:, c(i)), Theta.sigmar(c(i)), Gamma.Pr(:, :, c(i)), ur, Gamma.gammar(c(i)), lambdar, y(i));
+    % Update parameters denoted by r
+    [Theta.etar(:, c(i)), Theta.sigmar(c(i)), Gamma.Pr(:, :, c(i)), Gamma.gammar(c(i))] = update_parameters(Theta.etar(:, c(i)), Theta.sigmar(c(i)), Gamma.Pr(:, :, c(i)), Gamma.gammar(c(i)), lambdar, y(i+1), ur);
 end
 end
