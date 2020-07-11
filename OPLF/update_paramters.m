f182a203278ed9382cf27be746557749257c3923
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-11 11:11:58 +0200

    Update update_paramters.m

--- OPLF/update_paramters.m
+++ OPLF/update_paramters.m
@@ -1,10 +1,19 @@
-function [eta, sigma, P, gamma] = update_parameters(eta, sigma, P, u, gamma, lambda, s)
+function [eta, sigma, P, gamma] = update_parameters(eta, sigma, P, gamma, lambda, s, u)
+% This function updates recursively parameters eta and sigma and the state
+% variables P and gamma
+% u is the feature vector
+% lambda is the forgetting factor
+% s is the load
 % Avoid possible inestabilities of matrix P
-if norm(P) > 5
+if trace(P) > 10
     P = eye(length(P));
 end
+% Update matrix P
 P = (1/lambda)*(P - (P*u*u'*P)/(lambda + u'*P*u));
+% Update gamma
 gamma = 1 + lambda*gamma;
+% Update sigma
 sigma = sqrt(sigma^2 - (1/gamma)*(sigma^2 - lambda*(s - u'*eta)^2)/(lambda + u'*P*u));
+% Update eta
 eta = eta + (P*u/(lambda + u'*P*u))*(s - u'*eta);
 end
