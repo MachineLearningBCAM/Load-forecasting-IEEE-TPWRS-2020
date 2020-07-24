e258043a98974501676c74ef6b8df51e323292ca
VeronicaAlvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2020-07-24 09:43:18 +0200

    Update OPLF.m

--- OPLF/OPLF.m
+++ OPLF/OPLF.m
@@ -1,15 +1,29 @@
-function [RMSE, MAPE, predictions, load_demand] = OPLF(data, days_train, lambdad, lambdar, L, C, R)
-% [RMSE, MAPE, predictions, load_demand] = OPLF(data, 1, 0.2, 0.7, 24, 48, 3);
-% data is a Matlab struct such that:
-    % data.consumption is load demand
-    % data.c is the calendar information
-    % data.temperature is the temperature
-% days_train > 1 is the number of days that we will use for training
-% lambdad = 0.2 forgetting factor 
-% lambdar = 0.7 forgetting factor
-% L = 24 is the prediction horizon
-% C = 48 is the length of the calendar information
-% R = 3 is the length of the feature representation of observations
+% 
+% This file includes code for the paper Online Probabilistic Load Forecasting. 
+% @author: Verónica Álvarez
+% 
+function [RMSE, MAPE, predictions, load_demand, estimated_errors] = OPLF(data, days_train, lambdad, lambdar, L, C, R)
+% [RMSE, MAPE, predictions, load_demand, estimated_errors] = OPLF(data, 1, 0.2, 0.7, 24, 48, 3);
+%
+% Inputs
+    % data is a Matlab struct such that:
+        % data.consumption is load demand
+        % data.temperature is the temperature
+        % data.c is the calendar information
+    % days_train > 1 is the number of days that we will use for training
+    % lambdad = 0.2 forgetting factor 
+    % lambdar = 0.7 forgetting factor
+    % L = 24 is the prediction horizon
+    % C = 48 is the length of the calendar information
+    % R = 3 is the length of the feature representation of observations
+% 
+% Outputs
+    % RMSE is the root mean square error
+    % MAPE is the mean average percentage error
+    % predictions are load forecasts
+    % load_demand are real loads corresponding with load forecasts
+    % estimated_errors are the estimatation of the accuracy of load forecasts
+%
 % The mean of the temperatures that is included in the instance vector is
 % updated together with model parameters
 % Number of samples
