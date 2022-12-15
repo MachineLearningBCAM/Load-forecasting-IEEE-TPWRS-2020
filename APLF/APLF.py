1af59d2d2005efc5bae79550e3bb4ca8c948d107
Verónica Álvarez <67601462+VeronicaAlvarez@users.noreply.github.com>
2022-12-15 11:12:16 +0100

    Update APLF.py

--- APLF/APLF.py
+++ APLF/APLF.py
@@ -10,7 +10,7 @@ from datetime import datetime, date, time
 import csv
 #import pandas as pd
 # data in .mat file
-path = '../Example/'
+path = '../'
 # os.chdir(path)
 filename = '400buildings.mat'
 mat = loadmat(path + filename)  # load mat-file
@@ -21,7 +21,7 @@ data = {n: mdata[n][0, 0] for n in mdtype.names}
 # [MAPE, RMSE, predictions, load_demand, estimated_errors] = APLF(data, 300, 0.2, 0.7, 24, 48, 3)
 
 def initialize(C, R):
-  # initialize parameters
+  # initialize parameters
   import numpy as np
   class Theta:
     pass
@@ -47,22 +47,22 @@ def initialize(C, R):
   return a, b
 
 def update_parameters(eta, sigma, P, gamma, l, s, u):
-  import numpy
   if np.size(P) > 1:
     if P.trace() > 10:
       P = np.eye(len(P))
     P = (1/l)*(P - (np.dot(np.dot(np.dot(P, u), u.T), P)/(l + np.dot(np.dot(u.T, P),u))))
     gamma = 1 + l*gamma
-    sigma = np.sqrt(sigma**2 - (1/gamma)*(sigma**2 - l*(s - np.dot(eta,u))**2)/(l + np.dot(np.dot(u.T, P),u)))
-    eta = eta + (s - np.dot(u.T, eta))*np.dot(P, u).T[0]/(l + np.dot(np.dot(u.T, P),u))
+    sigma = np.sqrt(sigma**2 - (1/gamma)*(sigma**2 - l*(s - np.dot(u.T,eta)[0])**2)/(l + np.dot(np.dot(u.T, P)[0],u)[0]))
+    sigma = float(sigma)
+    eta = eta + (np.dot(P, u)/(l + np.dot(np.dot(u.T, P)[0],u)[0]))*(s - np.dot(u.T, eta)[0])
   else:
     if P > 10:
       P = 1
     P = (1/l)*(P - (P*u*np.transpose(u)*P)/(l + np.transpose(u)*P*u))
     gamma = 1 + l*gamma
-    sigma = np.sqrt(sigma**2 - (1/gamma)*(sigma**2 - l*(s - u*eta)**2)/(l + u*P*u))
-    eta = eta + (P*u/(l + u*P*u))*(s - u*eta)
-  return eta.T, sigma, P, gamma
+    sigma = np.sqrt(sigma**2 - (1/gamma)*(sigma**2 - l*(s - np.transpose(u)*eta)**2)/(l + np.transpose(u)*P*u))
+    eta = eta + (P*u/(l + np.transpose(u)*P*u))*(s - np.transpose(u)*eta)
+  return eta, sigma, P, gamma
 
 def test(predictions, load_demand):
   n = len(predictions)
@@ -79,7 +79,12 @@ def update_model(Theta, Gamma, y, x, c, lambdad, lambdar):
   s0 = x[0]
   w = x[1:]
   L = len(y)
-  y = [s0, y[0:]]
+  y = [[s0], y[0:]]
+  flat_list = []
+  for sublist in y:
+      for item in sublist:
+          flat_list.append(item[0])
+  y = flat_list
   for i in range(L):
     [Theta.wt[0][c[i]], Theta.sigmat[0,c[i]], Gamma.Pt[0,c[i]], Gamma.gammat[0,c[i]]] = update_parameters(Theta.wt[0,c[i][0]], Theta.sigmat[0,c[i][0]], Gamma.Pt[0,c[i][0]], Gamma.gammat[0, c[i][0]], 1, w[0][i][0], 1)
     if Theta.wt[0][c[i]] - w[0][i][0] > 20 and (w[0][i][0] > 80 or w[0][i][0] < 20):
@@ -92,16 +97,16 @@ def update_model(Theta, Gamma, y, x, c, lambdad, lambdar):
       alpha1 = 0
       alpha2 = 0
     ud = np.ones((2, 1))
-    ud[1, 0] = y[1][i][0]
-    [Theta.etad[0:, c[i]], Theta.sigmad[0, c[i][0]], Gamma.Pd[c[i][0]], Gamma.gammad[0, c[i][0]]] = update_parameters(Theta.etad[0:, c[i][0]], Theta.sigmad[0, c[i][0]], Gamma.Pd[c[i][0]], Gamma.gammad[0, c[i][0]], lambdad, y[1][i], ud)
+    ud[1, 0] = y[i]
+    [Theta.etad[0:, c[i]], Theta.sigmad[0, c[i][0]], Gamma.Pd[c[i][0]], Gamma.gammad[0, c[i][0]]] = update_parameters(Theta.etad[0:, c[i]], Theta.sigmad[0, c[i][0]], Gamma.Pd[c[i][0]], Gamma.gammad[0, c[i][0]], lambdad, y[i+1], ud)
     ur = np.ones((3, 1))
     ur[1, 0] = alpha1
     ur[2, 0] = alpha2
-    [Theta.etar[0:, c[i]], Theta.sigmar[0][c[i]], Gamma.Pr[c[i][0]], Gamma.gammar[0][c[i]]] = update_parameters(Theta.etar[0:, c[i][0]], Theta.sigmar[0][c[i][0]], Gamma.Pr[c[i][0]], Gamma.gammar[0][c[i][0]], lambdar, y[1][i], ur)
+    [Theta.etar[0:, c[i]], Theta.sigmar[0][c[i]], Gamma.Pr[c[i][0]], Gamma.gammar[0][c[i]]] = update_parameters(Theta.etar[0:, c[i]], Theta.sigmar[0][c[i][0]], Gamma.Pr[c[i][0]], Gamma.gammar[0][c[i][0]], lambdar, y[i+1], ur)
   return Theta, Gamma
 
 def prediction(theta, x, C):
-  # prediction function
+  # prediction function
   L = len(x[1])
   pred_s = np.zeros((L+1, 1))
   e = np.zeros((L+1, 1))
@@ -122,19 +127,20 @@ def prediction(theta, x, C):
       alpha2 = 0
     ur = np.transpose([1, alpha1, alpha2])
     pred_s[i+1, 0] = (np.dot(np.transpose(ud), theta.etad[0:, c])*theta.sigmar[0][c]**2 + np.dot(np.transpose(ur), theta.etar[0:, c])*(theta.sigmad[0][c]**2 + np.dot(np.dot([0, 1], theta.etad[0:, c])**2, e[i]**2)))/(theta.sigmar[0][c]*theta.sigmar[0][c] + theta.sigmad[0][c]**2 + np.dot((np.dot([0, 1], theta.etad[0:, c])**2),e[i]**2))
-    e[i+1, 0] = np.sqrt((theta.sigmar[0][c]**2 * (theta.sigmad[0][c]**2 + np.dot(np.dot(np.dot([0, 1], theta.etad[0:, c])**2, e[i]**2)))/(theta.sigmar[0][c]**2 + theta.sigmad[0][c]**2 + np.dot(np.dot([0, 1], theta.etad[0:, c])**2, e[i]**2))
+    e[i+1, 0] = np.sqrt((theta.sigmar[0][c]**2 * (theta.sigmad[0][c]**2 + np.dot(np.dot([0, 1], theta.etad[0:, c])**2, e[i]**2)))/(theta.sigmar[0][c]**2 + theta.sigmad[0][c]**2 + np.dot(np.dot([0, 1], theta.etad[0:, c])**2, e[i]**2)))
   return pred_s[1:], e[1:]
 
-# def APLF(data, days_train, lambdad, lambdar, L, C, R):
+# def APLF(data, days_train, lambdad, lambdar, L, C, R):
   # [MAPE, RMSE, predictions, load_demand, estimated_errors] = APLF(data, 300, 0.2, 0.7, 24, 48, 3)
   # days_train > 1 number of training days
 days_train = 300
-lambdad = 0.2 # forgetting factor
-lambdar = 0.7 # forgetting factor
-L = 24 # prediction horizon (hours)
-C = 48 # length of the calendar information
-R = 3 # length of feature vector of observations
+lambdad = 0.2 # forgetting factor
+lambdar = 0.7 # forgetting factor
+L = 24 # prediction horizon (hours)
+C = 48 # length of the calendar information
+R = 3 # length of feature vector of observations
 n = len(data.get('consumption'))
+# consumption = data.get('consumption')
 consumption = data.get('consumption')
 ct = data.get('c')
 ct = ct - 1
@@ -144,7 +150,7 @@ n_train = 24*days_train
 predictions = []
 estimated_errors = []
 load_demand = []
-for i in range(n_train - L+1):
+for i in range(0, n_train - L, L):
   s0 = consumption[i]
   w = temperature[i+1:i+L+1]
   x = [s0, w]
@@ -173,4 +179,4 @@ with open('results.csv', 'w+') as file:
         rcount = rcount + 1
     file.close()
 
-#  return MAPE, RMSE, predictions, load_demand, estimated_errors
+#  return MAPE, RMSE, predictions, load_demand, estimated_errors
